522d7262419159ba0716dedb5ac544da1867e342
fantasystar94 <aniseed_3@naver.com>
2025-10-14 20:09:05 +0900

    lv1 계산기 보완

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,45 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a=0;
+        int b=0;
         boolean exit = true;
-
+            while(exit){
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                a =sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                b= sc.nextInt();
+                if(a<0 || b<0){
+                    System.out.println("0 혹은 양의 정수를 입력해주세요");
+                    continue;
+                }
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                String c = sc.next();
+                if(c.equals("exit")){
+                    exit = false;
+                    break;
+                }
+                switch (c){
+                case "+": System.out.println(a+b);
+                break;
+                case "-" : System.out.println(a-b);
+                break;
+                case "*" : System.out.println(a*b);
+                break;
+                case "/" :
+                    if(b==0){
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
+                    System.out.println(a/b);
+                break;
+                default:
+                System.out.println("잘못된 연산자 입니다.");
+                break;
+                }
+            }
     }
 }
-//
-//                switch (c){
-//        case "+": System.out.println(a+b);
-//                        break;
-//                                case "-" : System.out.println(a-b);
-//                        break;
-//                                case "*" : System.out.println(a*b);
-//                        break;
-//                                case "/" : System.out.println(a/b);
-//                        break;
-//default:
-//        System.out.println("잘못된 연산자 입니다.");
-//                        break;
-//                                }
