a7f84f23db0cb5ff1863d5b329752b6f468b6df3
fantasystar94 <aniseed_3@naver.com>
2025-10-14 17:06:11 +0900

    LV1 계산기 다시작성. main 브랜치 리셋

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        String c = sc.next();
-        System.out.println(a+b+""+c);
+        boolean exit = true;
+
     }
 }
+//
+//                switch (c){
+//        case "+": System.out.println(a+b);
+//                        break;
+//                                case "-" : System.out.println(a-b);
+//                        break;
+//                                case "*" : System.out.println(a*b);
+//                        break;
+//                                case "/" : System.out.println(a/b);
+//                        break;
+//default:
+//        System.out.println("잘못된 연산자 입니다.");
+//                        break;
+//                                }
