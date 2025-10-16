a10b4dc13bc3dd8441a0696048c8a16eb258b222
fantasystar94 <aniseed_3@naver.com>
2025-10-16 21:38:58 +0900

    LV2 calculator 컬렉션 추가

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,27 +1,48 @@
 //10.15 LV2 시작
 
+import java.util.ArrayList;
+
 //1. 사칙연산 수행 후, 결과값 반환하는 메서드 구현
 //2. 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스 생성
 //3. 0을 포함한 양의 정수 2개와 연산 기호를 매개변수로 받아 사칙연산 수행
-//4. 이후 결과값을 반환하는 메서드와 연산결과를 저장하는 컬렉션 타입 필드를 가진 클래스 생성
+//4. 이후 결과값을 반환하는 메서드와
+//10.16 추가 작업 내용
+//TODO 5. 연산결과를 저장하는 컬렉션 타입 필드를 가진 클래스 생성
+//TODO 6. 연산 결과는 calculator 클래스의 연산 결과를 저장하는 필드에 저장
+//TODO 7. /App(현재 Main) 클래스의 main 메서드에서 calculator 클래스의 연산결과를 저장하고있는 컬렉션 필드에 직접 접근못하게 수정
+//TODO 8. calculator 클래스에 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현한 후 App 클래스의 main 메서드에 삭제 메서드 활용
 public class Calculator {
-    public int calculate(int a, int b,String c) {
-        switch (c){
-            case "+": System.out.println(a+b);
-                break;
-            case "-" : System.out.println(a-b);
-                break;
-            case "*" : System.out.println(a*b);
-                break;
-            case "/" :
-                if(b==0){
+
+    private void saveResult(int a) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        resultList.add(a);
+    }
+
+    /**
+     *
+     * @param a;
+     * @param b;
+     * @param c;
+     * @return result;
+     */
+    public int calculate(int a, int b, String c) {
+        int result = 0;
+        switch (c) {
+            case "+": result = a + b;
+                return result;
+            case "-": result = a - b;
+                return result;
+            case "*": result = a * b;
+                return result;
+            case "/":
+                if (b == 0) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    return 0;
                 }
-                System.out.println(a/b);
-                break;
+                result = a / b;
+                return result;
             default:
-                System.out.println("잘못된 연산자 입니다.");
-                break;
+                return 0;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,40 @@
 import java.util.Scanner;
 
+//LV2 추가 구현내용
+//연산 수행 역할은 calculator 클래스가 담당
+//연산 결과는 calculator 클래스의 연산 결과를 저장하는 필드에 저장
+//App(현재 Main) 클래스의 main 메서드에서 calculator 클래스의 연산결과를 저장하고있는 컬렉션 필드에 직접 접근못하게 수정
+//calculator 클래스에 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현한 후 App 클래스의 main 메서드에
+//삭제 메서드가 활용될 수 있도록 수정
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int a=0;
-        int b=0;
+        Calculator calculator = new Calculator();  //1. 생성자를 빈값으로 호출하거나. 2. 이미 호출할때 파라미터가 빈값이거나.
+        int a = 0;
+        int b = 0;
         boolean exit = true;
-            while(exit){
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                a =sc.nextInt();
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                b= sc.nextInt();
-                if(a<0 || b<0){
-                    System.out.println("0 혹은 양의 정수를 입력해주세요");
-                    continue;
-                }
-                System.out.print("사칙연산 기호를 입력하세요: ");
-                String c = sc.next();
-                if(c.equals("exit")){
-                    exit = false;
-                    break;
-                }
-                switch (c){
-                case "+": System.out.println(a+b);
-                break;
-                case "-" : System.out.println(a-b);
-                break;
-                case "*" : System.out.println(a*b);
-                break;
-                case "/" :
-                    if(b==0){
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        continue;
-                    }
-                    System.out.println(a/b);
-                break;
-                default:
-                System.out.println("잘못된 연산자 입니다.");
-                break;
-                }
+        while (exit) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            a = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            b = sc.nextInt();
+            if (a < 0 || b < 0) {
+                System.out.println("0 혹은 양의 정수를 입력해주세요");
+                continue;
             }
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String c = sc.next();
+            int result = calculator.calculate(a, b, c);
+            //리절트가 뭔지 검증을 더 해보는 구문...
+            System.out.println(result);
+
+            if(c.equals("exit")){
+                exit = false;
+                break;
+            }
+
+        }
     }
 }
