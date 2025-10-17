0f87052d344d50a472d40d92e276310dbc5f40c5
fantasystar94 <aniseed_3@naver.com>
2025-10-17 11:51:16 +0900

    LV2 calculator 메인에 메소드 추가하기

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 //App(현재 Main) 클래스의 main 메서드에서 calculator 클래스의 연산결과를 저장하고있는 컬렉션 필드에 직접 접근못하게 수정
 //calculator 클래스에 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현한 후 App 클래스의 main 메서드에
 //삭제 메서드가 활용될 수 있도록 수정
+//Todo. 10/17 연산자 입력받는 부분 메소드로 분리 후 예외처리 추가하기
 public class Main {
 
     public static void main(String[] args) {
@@ -29,6 +30,7 @@ public class Main {
             int result = calculator.calculate(a, b, c);
             //리절트가 뭔지 검증을 더 해보는 구문...
             System.out.println(result);
+            c = sc.next();
 
             if(c.equals("exit")){
                 exit = false;
