04d7b87b382f499dfbcf6e156a00aa807b24329a
fantasystar94 <aniseed_3@naver.com>
2025-10-17 13:15:43 +0900

    LV1 calculator 주석 수정

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 //App(현재 Main) 클래스의 main 메서드에서 calculator 클래스의 연산결과를 저장하고있는 컬렉션 필드에 직접 접근못하게 수정
 //calculator 클래스에 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현한 후 App 클래스의 main 메서드에
 //삭제 메서드가 활용될 수 있도록 수정
-//Todo. 10/17 연산자 입력받는 부분 메소드로 분리 후 예외처리 추가하기
+//10/17 연산자 입력받는 부분 메소드로 분리 후 예외처리 추가하기 - 완료
 public class Main {
     //인자값을 받아서, try-catch 로 정수일 경우에만 리턴해주기.
     public static int setInt(Scanner sc) {
-        System.out.print("정수를 입력해주세요 : ");
 
+        System.out.print("정수를 입력해주세요 : ");
         int a = 0;
+
         while(true){
             try{
                 a = sc.nextInt();
@@ -24,6 +25,7 @@ public class Main {
                 sc.nextLine();
             }
         }
+
         System.out.println(a);
         return a;
     }
@@ -32,20 +34,19 @@ public class Main {
         System.out.print("사칙연산 기호를 입력하세요 (+ - * /): ");
         String str;
         while(true){
-
-                try{
-                    str = sc.next();
-                    if(str.equals("+")||str.equals("-")||str.equals("*")||str.equals("/")){
-                        break;
-                    }
-                    else{
-                        System.out.println("사칙연산 기호를 입력해주세요 (+ - * /). 이외 글자들은 입력할 수 없습니다.");
-                    }
+            try{
+                str = sc.next();
+                if(str.equals("+")||str.equals("-")||str.equals("*")||str.equals("/")){
+                    break;
                 }
-                catch(InputMismatchException e){
-                    System.out.println("사칙연산 기호를 입력해주세요. 숫자는 입력할 수 없습니다.");
-                    sc.nextLine();
+                else{
+                    System.out.println("사칙연산 기호를 입력해주세요 (+ - * /). 이외 글자들은 입력할 수 없습니다.");
                 }
+            }
+            catch(InputMismatchException e){
+                System.out.println("사칙연산 기호를 입력해주세요. 숫자는 입력할 수 없습니다.");
+                sc.nextLine();
+            }
         }
         System.out.println(str);
         return str;
@@ -56,9 +57,7 @@ public class Main {
         if(("/").equals(c) && b==0) {
             return false;
         }
-        else{
-            return true;
-        }
+        return true;
     }
 
     // exit 일 경우 종료, 아닐경우 계속하기
@@ -69,8 +68,8 @@ public class Main {
             System.out.println("프로그램을 종료합니다.");
             return false;
         }
-            sc.nextLine();
-            return true;
+        sc.nextLine();
+        return true;
     }
 
 
@@ -94,18 +93,13 @@ public class Main {
 
             //리절트가 뭔지 검증을 더 해보는 구문...
             boolean divideChk = divideCheck(a,b,c);
-            if(divideChk){
-                result = calculator.calculate(a, b, c);
-            }
-            else{
+            if(!divideChk){
                 System.out.println("0으로 나눌 수 없습니다. 처음으로 되돌아갑니다.");
                 continue;
             }
+            result = calculator.calculate(a, b, c);
             System.out.println(result);
             controller = controlCalcul(sc);
-
         }
-
-
     }
 }
